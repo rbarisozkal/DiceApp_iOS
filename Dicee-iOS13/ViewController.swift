b700a5a4257b37a35af090deceadbeda3f3a452e
rbarisozkal <barisozkal11@gmail.com>
2023-10-14 22:21:40 +0300

    dice app finished

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func rollTheDice(_ sender: UIButton) {
+        let dices = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+        dice1
+            .image=UIImage(imageLiteralResourceName:NSDataAssetName(dices.randomElement() ?? ""))
+        dice2.image=UIImage(imageLiteralResourceName:NSDataAssetName(dices.randomElement() ?? ""))
+        print("clicked")
+    }
+    
 }
 
